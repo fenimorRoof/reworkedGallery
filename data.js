935fa2c5af32cebfc8d73c61eaba69341041c937
fenimorRoof <fenimo2roof@gmail.com>
2017-11-01 15:07:49 +0200

    The first frame

---
+++ data.js
@@ -0,0 +1,9 @@
+window.images = [{title: 'Yoda',
+description: 'Master Yoda is among the oldest known Jedi Master',
+src: './images/yoda.jpg'},{title: 'Obi Wan Kenobi',
+description: 'Anakins teacher ',
+src: './images/obi_wan.jpeg'},{title: 'Qui-Gon Jinn',
+description: 'Obi Wans teacher',
+src: './images/q-gon.jpg'},{title: 'Luke Skywalker',
+description: 'Anakins son',
+src: './images/images/luke_skywalker.jpg'}];
