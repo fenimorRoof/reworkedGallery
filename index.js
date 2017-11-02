14ace6634030163c2df17d1a59377c3dca8f2008
fenimorRoof <fenimo2roof@gmail.com>
2017-11-02 19:22:10 +0200

    Trash upd

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 $(document).ready(function() {
+
+  let slideNumber = 0;
+  function turnThePage(number) {
+    return window.images[number].src;
+  };
+
+  function renderImage() {
+    let slide = window.images[slideNumber];
+    $("body").append(`<img src='${turnThePage(slideNumber)}'>`);
+
+  };
+
+  function renderPagination() {
+    $("body").append("<button id='Prev' type='button'><</button>");
+    $("body").append("<button id='Next' type='button'>></button>");
+    $("#Next").click(function () {
+      slideNumber += 1;
+      console.log(slideNumber);
+      $("img").remove();
+      renderImage();
+    })
+  };
+
+
+  function init() {
+    renderImage();
+    renderPagination();
+  };
+  init();
 });
+ e3
