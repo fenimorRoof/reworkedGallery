21e816de2c215d31f245015dbaef38a084ba6c05
fenimorRoof <fenimo2roof@gmail.com>
2017-11-08 23:09:31 +0200

    fix(pagination) add current slide buttons

--- data.js
+++ data.js
@@ -1,9 +1,10 @@
-window.images = [{title: 'Yoda',
-description: 'Master Yoda is among the oldest known Jedi Master',
-src: './images/yoda.jpg'},{title: 'Obi Wan Kenobi',
-description: 'Anakins teacher ',
-src: './images/obi_wan.jpeg'},{title: 'Qui-Gon Jinn',
-description: 'Obi Wans teacher',
-src: './images/q-gon.jpg'},{title: 'Luke Skywalker',
-description: 'Anakins son',
-src: './images/luke_skywalker.jpg'}];
+window.images = [{
+  title: 'Yoda',
+  description: 'Master Yoda is among the oldest known Jedi Master',
+  src: './images/yoda.jpg'},{title: 'Obi Wan Kenobi',
+  description: 'Anakins teacher ',
+  src: './images/obi_wan.jpeg'},{title: 'Qui-Gon Jinn',
+  description: 'Obi Wans teacher',
+  src: './images/q-gon.jpg'},{title: 'Luke Skywalker',
+  description: 'Anakins son',
+  src: './images/luke_skywalker.jpg'}];
--- index.js
+++ index.js
@@ -1,58 +1,76 @@
 $(document).ready(function() {
 
-  let slideNumber = 0;
+  let slideNumber = 1;
   let arreyLength = window.images.length;
 
   //Rendering
   function renderImage() {
-    $("#image").html('');
-    let srcImage = window.images[slideNumber];
-    $("#image").append(`<img src="${srcImage.src}">`);
-  };
+    $('#image').html(''); //Delete previous image
+
+    let srcImage = window.images[slideNumber-1];
+    $('#image').append(`<img src='${srcImage.src}'>`);
+  }
 
   function renderPagination() {
-    $("#pagination").append("<button id='prev' type='button'>◀</button>");
-    $("#pagination").append("<button id='next' type='button'>▶</button>");
-    $("#prev").on("click", prevImage);
-    $("#next").on("click", nextImage);
-  };
+    $('#pagination').append('<button id="prev" type="button">◀</button>');
+    $('#pagination').append('<button id="next" type="button">▶</button>');
+    $('#pagination').append('<div id="slideButtons"></div>');
+
+    for(let i = 1; i <= arreyLength; i+= 1) {
+
+      //"i+1" for butons names and just i for slideNumber variable
+
+      $('#slideButtons').append(`<button type="button" value="slide-${i}">${i}</button>`);
+      $(`#slideButtons button[value="slide-${i}"]`).click(
+        (function(index){
+          return function() { changeSlide(index); } }
+        )(i)
+      );
+    }
+
+    $('#prev').on('click', prevImage);
+    $('#next').on('click', nextImage);
+  }
 
 
   //Actions
-
-  function changeSlide() {
-    renderImage();
-    updatePagination();
-  };
-
   function prevImage() {
-    slideNumber -= 1;
-    changeSlide();
-  };
+    changeSlide(slideNumber - 1);
+  }
 
   function nextImage() {
-    slideNumber += 1;
-    changeSlide();
-  };
+    changeSlide(slideNumber + 1);
+  }
+
+  function changeSlide(slide) {
+    slideNumber = slide;
+    renderImage();
+    updatePagination();
+  }
 
   function updatePagination() {
-    $("#pagination button[disabled]").prop('disabled', false);
 
-    if ((+slideNumber - 1) < 0) {
-      $("#pagination #prev").prop('disabled', true);
+    $('#pagination button[disabled]').prop('disabled', false);
+
+    $(`#pagination button[value="slide-${slideNumber}"]`).prop('disabled', true);
+
+    if (+slideNumber == 1) {
+      $('#pagination #prev').prop('disabled', true);
     };
 
-    if ((+slideNumber + 1) == arreyLength) {
-      $("#pagination #next").prop('disabled', true);
-    };
-  };
+    if (+slideNumber == arreyLength) {
+      $('#pagination #next').prop('disabled', true);
+    }
+  }
+
+
 
   //Initialization
   function init() {
     renderImage();
     renderPagination();
     updatePagination();
-  };
+  }
 
   init();
 });
